3b07bceed7d8a53c3830252c688dc4ff73278747
myky034 <vumyky.034@gmail.com>
2024-10-12 12:22:05 +0700

    update code

---
+++ src/app/(dashboard)/dashboard.tsx
@@ -0,0 +1,16 @@
+//"use client";
+
+import { Fragment, useState } from "react";
+import Link from "next/link";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <p>This is Dashboard</p>
+    </div>
+  );
+};
+
+export default Dashboard;
